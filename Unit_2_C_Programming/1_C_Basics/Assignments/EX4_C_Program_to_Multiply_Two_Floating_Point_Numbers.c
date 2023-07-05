2f75beb4480a9d2b43e7bd797ee01e6b045d20f4
Ahmed Shaddad <ahmedshaddad44@gmail.com>
2023-07-05 18:27:55 +0300

    edit in EX4

--- Unit_2_C_Programming/1_C_Basics/Assignments/EX4_C_Program_to_Multiply_Two_Floating_Point_Numbers.c
+++ Unit_2_C_Programming/1_C_Basics/Assignments/EX4_C_Program_to_Multiply_Two_Floating_Point_Numbers.c
@@ -3,7 +3,8 @@
  Name        : EX1_C_Program_To_Print_Sentence.c
  Author      : Ahmed Shaddad
  Created on	 : Jul 5, 2023
- Description : Unit 2 C-programming >> Assignment 1 C-Basic >> EX3 C Program to Add Two Integers using Ansi-style (c99)
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX4 C Program to Multiply two Floating Point
+ 	 	 	   Numbers using Ansi-style(c99) 
  ======================================================================================================================
  */
 
